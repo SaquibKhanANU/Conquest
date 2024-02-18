c490c7df5d2d8dcc10398d612519d6b655dfb640
Saquib Khan <u7108513@anu.edu.au>
2024-02-18 21:58:31 +1100

    profile built and current user reducer added

--- conquest-frontend/src/components/game-browser/Profile.js
+++ conquest-frontend/src/components/game-browser/Profile.js
@@ -1,10 +1,27 @@
 import "./Profile.css";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+    const currentPlayer = useSelector((state) => state.currentPlayer.player);
+
     return (
         <div className="profile-container">
-            <div>PROFILE</div>
+            <div className="profile-header">PROFILE</div>
+            <div className="profile-body">
+                <div className="profile-info">
+                    <p>Username:</p>
+                    <p>{currentPlayer.playerName}</p>
+                </div>
+                <div className="profile-info">
+                    <p>Games played:</p>
+                    <p>0</p>
+                </div>
+                <div className="profile-info">
+                    <p>Games won:</p>
+                    <p>0</p>
+                </div>
+            </div>
         </div>
     );
 };
